fa30a4dd13192dae720511ebf000394f3810e888
Travis Toal <travisty12@gmail.com>
2019-03-29 15:13:34 -0700

    create modal object

---
+++ src/app/body-div/mock-modals.ts
@@ -0,0 +1,10 @@
+import { Modal } from './modal.ts';
+
+export const MODALS: Modal[] = [
+  { source: 'space', overlay: 'CTRL-H HACKERSPACE' },
+  { source: 'woodshop', overlay: 'WOOD SHOP' },
+  { source: 'backlot', overlay: 'THE BACK LOT' },
+  { source: 'electronics', overlay: 'ELECTRONICS LAB' },
+  { source: 'secret', overlay: 'TOP SECRET LAB' },
+  { source: 'craft', overlay: 'CRAFT LAB' }
+]
